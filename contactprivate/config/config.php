4a27dd766b3b6cb6ba81ea602f0ce892e4a2e758
a.makovetskiy <a.makovetskiy@a1-systems.ru>
2014-02-25 12:47:55 +0400

    fix version and configs

--- contactprivate/config/config.php
+++ contactprivate/config/config.php
@@ -7,6 +7,6 @@ $config = array();
 
 $config['db']['table']['user_private_setting'] = '___db.table.prefix___private_setting';
 
-$config['default'] = 'all';
+$config['default'] = 'all'; //all,registered,friends
 
 return $config;
